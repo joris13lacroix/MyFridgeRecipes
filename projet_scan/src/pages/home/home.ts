f0c45cf7f735407a451db9bbab456cbcabc5f484
Unknown <joris13lacroix@gmail.com>
2018-02-12 09:55:48 +0100

    Auto stash before merge of "master" and "origin/master"

--- projet_scan/src/pages/home/home.ts
+++ projet_scan/src/pages/home/home.ts
@@ -7,15 +7,30 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+<<<<<<< Updated upstream
   recipe = {}
+=======
+
+  cucumber: String;
+  stat:boolean;
+>>>>>>> Stashed changes
 
   constructor(public navCtrl: NavController) {
-    
+    this.cucumber="concombre";
   }
 
+ 
   
+<<<<<<< Updated upstream
   goRecipe() {
     console.log(this.recipe)
   }
   
+=======
+    updateCucumber(monobjet,mystat) {
+      if (mystat==true){
+      console.log(monobjet);
+      }
+    }
+>>>>>>> Stashed changes
 }
